012f39fcab52fbfbf3c247ccd28e0eaf748ec2bb
KhalilKhunji <khalilkhunji@gmail.com>
2024-08-04 14:24:58 +0300

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const expenses = [
+    { item: 'Car', withinbudget: true },
+    { item: 'PC', withinbudget: false },
+    { item: 'Refrigerator', withinbudget: false },
+  ];
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>List of Expenses</h1>
+      <ul>
+        {expenses.map((expense, index) => (
+          <li key={index} className={expense.withinbudget ? 'within' : 'without'}>
+            {`${expense.item} ${expense.withinbudget ? 'is within budget!' : 'is above budget!'}`}
+          </li>
+        ))}
+      </ul>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
